f4034b1f076f4f22c75d0dd7cf08cc837802b078
EARLMATTHEWS <EarlMatthews@users.noreply.github.com>
2025-02-21 14:51:54 -0500

    Created support for a simple REPL

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 import(
+	"bufio"
+	"os"
 	"fmt"
 	"strings"
 )
@@ -11,5 +13,16 @@ func cleanInput(text string) []string {
 }
 
 func main(){
-	fmt.Println( "Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan(){
+			break
+		}
+		input := strings.TrimSpace(scanner.Text())
+		words := strings.Fields(strings.ToLower(input))
+		if len(words) > 0 {
+			fmt.Printf("Your command was: %s\n",words[0])
+		}
+	}
 }
